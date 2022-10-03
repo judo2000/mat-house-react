46fd3ba85b986d31b59da56d0aad58294dc22214
Stephen Moore <judo2000@comcast.net>
2022-10-03 12:02:41 -0600

    Update event card

--- client/src/pages/events/Events.js
+++ client/src/pages/events/Events.js
@@ -4,11 +4,18 @@ import { GET_EVENTS } from '../../utils/queries';
 import Loader from '../../components/Loader';
 import { Card, Col, Row } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
 
 const Events = () => {
   const { data, loading } = useQuery(GET_EVENTS);
 
   const eventsData = data?.events || {};
+
+  const convertDate = (date) => {
+    let tempDate = parseInt(date);
+    return moment(tempDate).add(1, 'days').format('MM-DD-YYYY');
+  };
   console.log(eventsData);
   return (
     <>
@@ -20,31 +27,46 @@ const Events = () => {
           {eventsData ? (
             eventsData.map((event, index) => {
               return (
-                <Col sm={12} md={3} key={index} className='pt-4'>
-                  <Card
-                    style={{
-                      height: '350px',
-                      border: 'solid #000000 1px',
-                      backgroundColor: 'lightgray',
-                      opacity: 0.8,
-                      background: `lightgray `,
-                    }}
-                  >
-                    <Card.Title className='pb-0'>
-                      {event.eventName}
-                      <br />
-                    </Card.Title>
-                    <Card.Body
-                      className='row align-items-end pt-0 text-center'
+                <Col sm={12} md={4} key={index} className='pt-4'>
+                  <Link to={event.slug} className='eventCard'>
+                    <Card
                       style={{
-                        background: `url(${event.logo}) center no-repeat`,
+                        height: '350px',
+                        border: 'solid #000000 1px',
+                        background: `lightgray`,
                       }}
                     >
-                      <NavLink className='text-black' to={event.slug}>
-                        Click here for details
-                      </NavLink>
-                    </Card.Body>
-                  </Card>
+                      <Card.Title
+                        style={{ opacity: 1 }}
+                        className='pb-0 text-center'
+                      >
+                        {event.eventName}
+                        <br />
+                      </Card.Title>
+                      <Card.Body className=' pt-0 text-center'>
+                        {convertDate(event.eventStartDate)}
+                        <br />
+                        {event.eventCity}, {event.eventState}
+                        <br />
+                        {event.logo ? (
+                          <>
+                            <img
+                              className='eventBgImage align-items-top'
+                              src={event.logo}
+                              style={{ opacity: 0.5, height: '150px' }}
+                              alt='Event Logo'
+                            />
+                            <br />
+                          </>
+                        ) : (
+                          <br />
+                        )}
+                        <NavLink className='text-black' to={event.slug}>
+                          Event Details
+                        </NavLink>
+                      </Card.Body>
+                    </Card>
+                  </Link>
                 </Col>
               );
             })
